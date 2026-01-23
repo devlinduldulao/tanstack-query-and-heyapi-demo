53064d65188f70d214c4f3f81b76d72c4de6f5e1
Devlin Duldulao <webmasterdevlin@gmail.com>
2026-01-23 14:32:19 +0100

    Update MainNav to reflect the year 2026 for React Miami

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -17,7 +17,7 @@ export function MainNav({ className, ...props }: React.HTMLAttributes<HTMLElemen
       >
         {({ isActive }) => (
           <>
-            React Miami 26
+            React Miami 2026
             {isActive && (
               <motion.div
                 layoutId="nav-indicator"
